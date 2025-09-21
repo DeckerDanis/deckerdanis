bc5a8af97a334b67de40d7ea2f7e6dda62f76dcd
moneynow78781 <moneynow78781@gmail.com>
2025-09-21 13:07:43 +0900

    sd

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // --- 이 부분을 추가하세요 ---
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has TypeScript errors.
-    ignoreBuildErrors: true,
-  },
-  // -------------------------
 };
 
 export default nextConfig;
